239f49195c8f47925f0b9370cfca7ad5d74b094b
nickkebbas <nicholas.kebbas@yahoo.com>
2015-02-16 16:21:34 -0800

    change web.js

--- web.js
+++ web.js
@@ -1,24 +1,8 @@
+var gzippo = require('gzippo');
 var express = require('express');
+var morgan = require('morgan');
 var app = express();
 
-// set the port of our application
-// process.env.PORT lets the port be set by Heroku
-var port = process.env.PORT || 8080;
-
-// set the view engine to ejs
-app.set('view engine', 'ejs');
-
-// make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/public'));
-
-// set the home page route
-app.get('/', function(req, res) {
-
-    // ejs render automatically looks in the views folder
-    res.render('index');
-});
-
-app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
-});
-
+app.use(morgan('dev'));
+app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.listen(process.env.PORT || 5000);
