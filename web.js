ecd305fd8c814a096e2c8f758eedefbe2920dffa
nickkebbas <nicholas.kebbas@yahoo.com>
2015-02-17 00:03:05 -0800

    make assets show up in production

--- web.js
+++ web.js
@@ -5,4 +5,7 @@ var app = express();
 
 app.use(morgan('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.use(express.static(__dirname + '/app'));
+//add this so the browser can GET the bower files
+app.use('/bower_components', express.static(__dirname + 'bower_components'));
 app.listen(process.env.PORT || 5000);
