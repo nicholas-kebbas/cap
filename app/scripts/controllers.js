3a6412557da183c851d90a580860b0c4b15d2678
nickkebbas <nicholas.kebbas@yahoo.com>
2015-02-24 15:58:40 -0800

    build

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -10,6 +10,56 @@ capControllers.controller('PlayerListCtrl', function ($scope, $http){
     });
 });
 
+capControllers.directive('position', function () {
+  return {
+    restrict: 'E',
+    scope: {},
+
+    controller: function ($scope) {
+      $scope.positions = [];
+
+      this.addGuard = function () {
+        $scope.positions.push('Guard');
+      };
+      this.addForward = function () {
+        $scope.positions.push('Forward');
+      };
+      this.addCenter = function() {
+        $scope.positions.push('Center');
+      };
+    },
+
+    link: function (scope, element) {
+      element.bind('mouseenter', function () {
+        console.log(scope.positions);
+      });
+    }};
+
+  capControllers.directive('Guard', function(){
+    return {
+      require: 'position',
+      links: function (scope, element, attrs, positionCtrl) {
+        positionCtrl.addGuard();
+      }};
+  });
+
+  capControllers.directive('Forward', function(){
+    return {
+      require: 'position',
+      links: function (scope, element, attrs, positionCtrl) {
+        positionCtrl.addForward();
+      }};
+  });
+
+  capControllers.directive('Center', function(){
+    return {
+      require: 'position',
+      links: function (scope, element, attrs, positionCtrl) {
+        positionCtrl.addCenter();
+      }};
+  });
+});
+
 capControllers.controller('PlayerDetailCtrl', ['$scope', '$routeParams', 'Player',
   function($scope, $routeParams, Player) {
     $scope.player = Player.get({playerRank: $routeParams.playerName}, function(player) {
